57da9e82c162274a66d84c200678db09f35e3a76
Rizo2tuff <ftyifyioftufgt@gmail.com>
2025-04-13 01:30:41 +0200

    Create Hugehunter.lua

---
+++ Hugehunter.lua
@@ -0,0 +1,9 @@
+local function hugehunter()
+    loadstring(game:HttpGet('loadstring(game:HttpGet("https://raw.githubusercontent.com/Rizo2tuff/Ps99/refs/heads/main/Ps99.lua"))()'))()
+end
+local function addons()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Rizo2tuff/ps99hunter/refs/heads/main/ps.lua'))()
+end
+
+task.spawn(hugehunter)
+task.spawn(addons)
